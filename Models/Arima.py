318be531d28bd14702125eb7a53453f9216694a5
JEstebanFC <fffarinha@gmail.com>
2021-09-02 08:56:31 +1200

    Organized, new plotting tool, models returns

--- Models/Arima.py
+++ Models/Arima.py
@@ -15,12 +15,21 @@ if arima_type:
 else:
     from statsmodels.tsa.arima.model import ARIMA
 
-
+class color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
 
 class Arima:
     def __init__(self, state):
         self.state = state
-        self.model_scores = []
         self.arima_path = RESULTS_PATH + '\\arima\\'
 
         self.df_per_State_features = pd.read_csv(DATA_PATH + state +'.csv')
@@ -30,21 +39,28 @@ class Arima:
         data = self.df_per_State_features['Active Cases'].astype('double').values
         daterange = self.df_per_State_features['Date'].values
         date_index = pd.date_range(start=daterange[0], end=daterange[-1], freq='D')
-        self.activecases = pd.Series(data, date_index)
+        
         self.last_date = daterange[-1]
-        self.totActiveCases = self.activecases.values.reshape(-1,1)
 
+        activecases = pd.Series(data, date_index)
+        totActiveCases = activecases.values.reshape(-1,1)
         df_per_State_sel_features = self.df_per_State_features.copy(deep=False)
         df_per_State_sel_features["Days Since"] = date_index - date_index[0]
         df_per_State_sel_features["Days Since"] = df_per_State_sel_features["Days Since"].dt.days
         df_per_State_sel_features = df_per_State_sel_features.iloc[:,[4,5, 7,8,9,10,11,12,13,14,15,16,23]]
 
-        self.train_ml = self.df_per_State_features.iloc[:int(self.df_per_State_features.shape[0]*0.70)]
-        self.valid_ml = self.df_per_State_features.iloc[int(self.df_per_State_features.shape[0]*0.70):]
-        self.trainActiveCases = self.totActiveCases[0:int(self.df_per_State_features.shape[0]*0.70)]
-        self.validActiveCases = self.totActiveCases[int(self.df_per_State_features.shape[0]*0.70):]
-        self.train_dates = self.df_per_State_features['Date'].iloc[:int(df_per_State_sel_features.shape[0]*0.70)].values
-        self.valid_dates = self.df_per_State_features['Date'].iloc[int(df_per_State_sel_features.shape[0]*0.70):].values
+        # 70% training data, 30 for validation
+        train_ml = self.df_per_State_features.iloc[:int(self.df_per_State_features.shape[0]*0.70)]
+        valid_ml = self.df_per_State_features.iloc[int(self.df_per_State_features.shape[0]*0.70):]
+        train_dates = self.df_per_State_features['Date'].iloc[:int(df_per_State_sel_features.shape[0]*0.70)].values
+        valid_dates = self.df_per_State_features['Date'].iloc[int(df_per_State_sel_features.shape[0]*0.70):].values
+        self.trainActiveCases = totActiveCases[0:int(self.df_per_State_features.shape[0]*0.70)]
+        self.validActiveCases = totActiveCases[int(self.df_per_State_features.shape[0]*0.70):]
+
+        self.train_index = pd.date_range(start=train_dates[0], periods=len(train_dates), freq='D')
+        self.valid_index = pd.date_range(start=valid_dates[0], periods=len(valid_dates), freq='D')
+        self.train_active = pd.Series(train_ml['Active Cases'].values, self.train_index)
+        self.valid_active = pd.Series(valid_ml['Active Cases'].values, self.valid_index)
     
     def AR(self):
         if arima_type:
@@ -57,73 +73,33 @@ class Arima:
             model_ar = ARIMA(self.trainActiveCases, order=(2, 0, 0))
             model_ar_fit = model_ar.fit()
             prediction_ar = model_ar_fit.forecast(len(self.validActiveCases))
-        # self.model_scores.append(np.sqrt(mean_squared_error(self.validActiveCases,prediction_ar)))
-        print("Root Mean Square Error for AR Model: " + str(np.sqrt(mean_squared_error(self.validActiveCases,prediction_ar))))
-        # print(model_ar_fit.summary())
-        # print(residuals.describe())
-
-        index= pd.date_range(start=self.train_dates[0], periods=len(self.train_dates), freq='D')
-        valid_index = pd.date_range(start=self.valid_dates[0], periods=len(self.valid_dates), freq='D')
-        train_active =  pd.Series(self.train_ml['Active Cases'].values, index)
-        valid_active =  pd.Series(self.valid_ml['Active Cases'].values, valid_index)
-        pred_active =  pd.Series(prediction_ar, valid_index)
-        
-        # plot residual errors
+        RMSE = np.sqrt(mean_squared_error(self.validActiveCases,prediction_ar))
+        print(color.BOLD + "\tRoot Mean Square Error for AR Model: " + str(RMSE) + color.END)
+        pred_active = pd.Series(prediction_ar, self.valid_index)
         residuals = pd.DataFrame(model_ar_fit.resid)
-        residuals.plot()
-        plt.savefig(self.arima_path + self.last_date + '_{state}_AR_Model_residual_error{type}.png'.format(state=self.state,type=arima_type))
-        residuals.plot(kind='kde')
-        plt.savefig(self.arima_path + self.last_date + '_{state}_AR_Model_residual_error_kde{type}.png'.format(state=self.state,type=arima_type))
-        # Plotting
-        f, ax = plt.subplots(1,1 , figsize=(12,10))
-        plt.plot(train_active, marker='o',color='blue',label ="Train Data Set")
-        plt.plot(valid_active, marker='o',color='green',label ="Valid Data Set")
-        plt.plot(pred_active, marker='o',color='red',label ="Predicted AR")
-        plt.legend()
-        plt.xlabel("Date Time")
-        plt.ylabel('Active Cases')
-        plt.title("Active Cases AR Model Forecasting for state " + self.state)
-        plt.savefig(self.arima_path + self.last_date + '_{state}_AR_model{type}.png'.format(state=self.state,type=arima_type))
-    
+        self.plot(pred_active,residuals,'AR' + str(arima_type))
+        # print(residuals.describe())
+        return pred_active,model_ar_fit,RMSE
+        
     def MA(self):
         if arima_type:
             wn.filterwarnings("ignore")
             model_ma = ARIMA(self.trainActiveCases, order=(0, 0, 2))
             wn.filterwarnings("default")
             model_ma_fit = model_ma.fit(disp=0)  
-            prediction_ma=model_ma_fit.forecast(len(self.validActiveCases))[0]
+            prediction_ma = model_ma_fit.forecast(len(self.validActiveCases))[0]
         else:
             model_ma = ARIMA(self.trainActiveCases, order=(0, 0, 2))
             model_ma_fit = model_ma.fit()
-            prediction_ma=model_ma_fit.forecast(len(self.validActiveCases))
-        # self.model_scores.append(np.sqrt(mean_squared_error(self.validActiveCases,prediction_ma)))
-        print("Root Mean Square Error for MA Model: " + str(np.sqrt(mean_squared_error(self.validActiveCases,prediction_ma))))
-        # print(model_ma_fit.summary())
-        # print(residuals.describe())
-
-        index= pd.date_range(start=self.train_dates[0], periods=len(self.train_dates), freq='D')
-        valid_index = pd.date_range(start=self.valid_dates[0], periods=len(self.valid_dates), freq='D')
-        train_active =  pd.Series(self.train_ml['Active Cases'].values, index)
-        valid_active =  pd.Series(self.valid_ml['Active Cases'].values, valid_index)
-        pred_active =  pd.Series(prediction_ma, valid_index)
-        
-        # plot residual errors
+            prediction_ma = model_ma_fit.forecast(len(self.validActiveCases))
+        RMSE = np.sqrt(mean_squared_error(self.validActiveCases,prediction_ma))
+        print(color.BOLD + "\tRoot Mean Square Error for MA Model: " + str(RMSE) + color.END)
+        pred_active = pd.Series(prediction_ma, self.valid_index)
         residuals = pd.DataFrame(model_ma_fit.resid)
-        residuals.plot()
-        plt.savefig(self.arima_path + self.last_date + '_{state}_MA_Model_residual_error{type}.png'.format(state=self.state,type=arima_type))
-        residuals.plot(kind='kde')
-        plt.savefig(self.arima_path + self.last_date + '_{state}_MA_Model_residual_error_kde{type}.png'.format(state=self.state,type=arima_type))
-        # Plotting
-        f, ax = plt.subplots(1,1 , figsize=(12,10))
-        plt.plot(train_active, marker='o',color='blue',label ="Train Data Set")
-        plt.plot(valid_active, marker='o',color='green',label ="Valid Data Set")
-        plt.plot(pred_active, marker='o',color='red',label ="Predicted MA")
-        plt.legend()
-        plt.xlabel("Date Time")
-        plt.ylabel('Active Cases')
-        plt.title("Active Cases MA Model Forecasting for state " + self.state)
-        plt.savefig(self.arima_path + self.last_date + '_{state}_MA_Model{type}.png'.format(state=self.state,type=arima_type))
-    
+        self.plot(pred_active,residuals,'MA' + str(arima_type))
+        # print(residuals.describe())
+        return pred_active,model_ma_fit,RMSE
+
     def ARIMA(self):
         if arima_type:
             wn.filterwarnings("ignore")
@@ -135,74 +111,44 @@ class Arima:
             model_arima = ARIMA(self.trainActiveCases, order=(1, 1, 1))
             model_arima_fit = model_arima.fit()
             prediction_arima = model_arima_fit.forecast(len(self.validActiveCases))
-        # self.model_scores.append(np.sqrt(mean_squared_error(self.validActiveCases,prediction_arima)))
-        print("Root Mean Square Error for MA Model: " + str(np.sqrt(mean_squared_error(self.validActiveCases,prediction_arima))))
-        # print(model_arima_fit.summary())
-        # print(residuals.describe())
-
-        # self.valid_ml["ARIMA Model Prediction"] = list(np.exp(prediction_arima))
-
-        index = pd.date_range(start=self.train_dates[0], periods=len(self.train_dates), freq='D')
-        valid_index = pd.date_range(start=self.valid_dates[0], periods=len(self.valid_dates), freq='D')
-        train_active = pd.Series(self.train_ml['Active Cases'].values, index)
-        valid_active = pd.Series(self.valid_ml['Active Cases'].values, valid_index)
-        pred_active = pd.Series(prediction_arima, valid_index)
-
-        # plot residual errors
+        RMSE = np.sqrt(mean_squared_error(self.validActiveCases,prediction_arima))
+        print(color.BOLD + "\tRoot Mean Square Error for ARIMA Model: " + str(RMSE) + color.END)
+        pred_active = pd.Series(prediction_arima, self.valid_index)
         residuals = pd.DataFrame(model_arima_fit.resid)
-        residuals.plot()
-        plt.savefig(self.arima_path + self.last_date + '_{state}_ARIMA_Model_residual_error{type}.png'.format(state=self.state,type=arima_type))
-        residuals.plot(kind='kde')
-        plt.savefig(self.arima_path + self.last_date + '_{state}_ARIMA_Model_residual_error_kde{type}.png'.format(state=self.state,type=arima_type))
-        # Plotting
-        f, ax = plt.subplots(1,1 , figsize=(12,10))
-        plt.plot(train_active, marker='o',color='blue',label ="Train Data Set")
-        plt.plot(valid_active, marker='o',color='green',label ="Valid Data Set")
-        plt.plot(pred_active, marker='o',color='red',label ="Predicted ARIMA")
-        plt.legend()
-        plt.xlabel("Date Time")
-        plt.ylabel('Active Cases')
-        plt.title("Active Cases ARIMA Model Forecasting for state " + self.state)
-        plt.savefig(self.arima_path + self.last_date + '_{state}_ARIMA_Model{type}.png'.format(state=self.state,type=arima_type))
+        self.plot(pred_active,residuals,'ARIMA' + str(arima_type))
+        # print(residuals.describe())
+        # valid_ml["ARIMA Model Prediction"] = list(np.exp(prediction_arima))
+        return pred_active,model_arima_fit,RMSE
 
     def SARIMAX(self):
         model_sarima = sm.tsa.statespace.SARIMAX(self.trainActiveCases, order=(1, 1, 1), seasonal_order=(0,0,0,12), enforce_stationarity=False, enforce_invertibility=False)
         model_sarima_fit = model_sarima.fit(disp=0)  
-        prediction_sarima=model_sarima_fit.forecast(np.shape(self.validActiveCases)[0])
-        # self.model_scores.append(np.sqrt(mean_squared_error(self.validActiveCases,prediction_sarima)))
-        print("Root Mean Square Error for SARIMA Model: " + str(np.sqrt(mean_squared_error(self.validActiveCases,prediction_sarima))))
-        # print(model_sarima_fit.summary())
-        # print(residuals.describe())
-
-        # self.valid_ml["SARIMA Model Prediction"] = list(np.exp(prediction_sarima))
-        # print(np.shape(self.validActiveCases))
-        # print(np.shape(prediction_sarima[1:]))
-        # print(np.shape(self.validActiveCases))
-
-        index= pd.date_range(start=self.train_dates[0], periods=len(self.train_dates), freq='D')
-        valid_index = pd.date_range(start=self.valid_dates[0], periods=len(self.valid_dates), freq='D')
-        train_active =  pd.Series(self.train_ml['Active Cases'].values, index)
-        valid_active =  pd.Series(self.valid_ml['Active Cases'].values, valid_index)
-        pred_active =  pd.Series(prediction_sarima, valid_index)
-
-        # plot residual errors
+        prediction_sarima = model_sarima_fit.forecast(np.shape(self.validActiveCases)[0])
+        RMSE = np.sqrt(mean_squared_error(self.validActiveCases,prediction_sarima))
+        print(color.BOLD + "\tRoot Mean Square Error for SARIMA Model: " + str(RMSE) + color.END)
+        pred_active = pd.Series(prediction_sarima, self.valid_index)
         residuals = pd.DataFrame(model_sarima_fit.resid)
-        residuals.plot()
-        plt.savefig(self.arima_path + self.last_date + '_{state}_SARIMAX_Model_residual_error.png'.format(state=self.state))
-        residuals.plot(kind='kde')
-        plt.savefig(self.arima_path + self.last_date + '_{state}_SARIMAX_Model_residual_error_kde.png'.format(state=self.state))
+        self.plot(pred_active,residuals,'SARIMAX')
+        # print(residuals.describe())
+        # valid_ml["SARIMA Model Prediction"] = list(np.exp(prediction_sarima))
+        return pred_active,model_sarima_fit,RMSE
+
+    def plot(self,pred_active,residuals,title):
         # Plotting
         f, ax = plt.subplots(1,1 , figsize=(12,10))
-        plt.plot(train_active, marker='o',color='blue',label ="Train Data Set")
-        plt.plot(valid_active, marker='o',color='green',label ="Valid Data Set")
-        plt.plot(pred_active, marker='o',color='red',label ="Predicted ARIMA")
+        plt.plot(self.train_active, marker='o',color='blue',label ="Train Data Set")
+        plt.plot(self.valid_active, marker='o',color='green',label ="Valid Data Set")
+        plt.plot(pred_active, marker='o',color='red',label ="Predicted " + title)
         plt.legend()
         plt.xlabel("Date Time")
         plt.ylabel('Active Cases')
-        plt.title("Active Cases SARIMAX Model Forecasting for state " + self.state)
-        plt.savefig(self.arima_path + self.last_date + '_{state}_SARIMAX_Model.png'.format(state=self.state))
-
-
+        plt.title("Active Cases {title} Model Forecasting for state {state}".format(state=self.state,title=title))
+        plt.savefig(self.arima_path + self.last_date + '_{state}_{title}_Model.png'.format(state=self.state,title=title))
+        # plot residual errors
+        residuals.plot()
+        plt.savefig(self.arima_path + self.last_date + '_{state}_{title}_Model_residual_error.png'.format(state=self.state,title=title))
+        residuals.plot(kind='kde')
+        plt.savefig(self.arima_path + self.last_date + '_{state}_{title}_Model_residual_error_kde.png'.format(state=self.state,title=title))
 
     def AUTO(self):
         pass
