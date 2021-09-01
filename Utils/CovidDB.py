d27a419c34c3ba09f560a187a171848fd7c2564c
JEstebanFC <fffarinha@gmail.com>
2021-09-01 16:32:11 +1200

    countryCases finish

--- Utils/CovidDB.py
+++ Utils/CovidDB.py
@@ -6,14 +6,29 @@ from datetime import datetime, timedelta
 # nz[nz.columns[-1]].iloc[-1] - nz[nz.columns[-2]].iloc[-1]
 # nz.iloc[-1,-1] - nz.iloc[-1,-2]
 
+class color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+
 class CovidDB:
     def __init__(self):
         url_confirmed = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
         url_deaths = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv'
         url_recovered = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv'
-        self.confirmed = pd.read_csv(url_confirmed)
-        self.deaths = pd.read_csv(url_deaths)
-        self.recovered = pd.read_csv(url_recovered)
+        url_vaccine_doses = 'https://raw.githubusercontent.com/govex/COVID-19/master/data_tables/vaccine_data/global_data/time_series_covid19_vaccine_doses_admin_global.csv'
+        url_vaccine_detail = 'https://raw.githubusercontent.com/govex/COVID-19/master/data_tables/vaccine_data/global_data/time_series_covid19_vaccine_global.csv'
+        
+        self.confirmed = pd.read_csv(url_confirmed, index_col=1)
+        self.deaths = pd.read_csv(url_deaths, index_col=1)
+        self.recovered = pd.read_csv(url_recovered, index_col=1)
         self.date_format = '%m/%d/%y'
         
     def countryCases(self, country, date):
@@ -24,9 +39,22 @@ class CovidDB:
         if lastDate[0] == '0':
             lastDate = lastDate[1:]
         
-        cases = self.confirmed.loc[self.confirmed[self.confirmed.columns[index]] == country]
-        dailyCases = cases[date] - cases[lastDate]
-        print('Confirmed cases in {country} between {lastDate} and {date}:'.format(country=country,lastDate=lastDate,date=date))
-        print(dailyCases,'\n')
-        return dailyCases
+        # confirmed_cases = self.confirmed.loc[self.confirmed[self.confirmed.columns[index]] == country]
+        confirmed_cases = self.confirmed.loc[country]
+        death_cases = self.deaths.loc[country]
+        recover_cases = self.recovered.loc[country]
+
+        daily_confirmed_cases = confirmed_cases[date] - confirmed_cases[lastDate]
+        daily_death_cases = death_cases[date] - death_cases[lastDate]
+        daily_recover_cases = recover_cases[date] - recover_cases[lastDate]
+
+        daily_cases = [confirmed_cases.iloc[:,0]]
+        daily_cases.append(pd.Series(daily_confirmed_cases,name='Confirmed'))
+        daily_cases.append(pd.Series(daily_death_cases,name='Death'))
+        daily_cases.append(pd.Series(daily_recover_cases,name='Recovered'))
+        
+        result = pd.concat(daily_cases, axis=1)
+        print(color.BOLD  + 'Cases on {date}:'.format(country=country,lastDate=lastDate,date=date) + color.END)
+        print(result.to_string(),'\n')
+        return daily_confirmed_cases,daily_death_cases,daily_recover_cases
 
