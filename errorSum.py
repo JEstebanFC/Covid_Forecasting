dd97f07565fc597694803ad4ac5b089dc146f2ba
Jaime Farina <fffarinha@gmail.com>
2022-05-01 15:11:38 +1200

    Multiple Error files summarize it

---
+++ errorSum.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import os
+import pandas as pd
+
+PATH = os.getcwd()
+
+ePath = PATH + '\\Results\\2022-04-29\\1st\\Errors\\'
+eFiles = os.listdir(ePath)
+index = ','.join(eFiles).replace('.csv','').split(',')
+
+dfs = []
+for ef in eFiles:
+    df = pd.read_csv(ePath + ef)
+    df.rename(columns={'Unnamed: 0': 'Metric'},inplace=True)
+    df.set_index(['Metric'],inplace=True)
+    dfs.append(df)
+
+result = pd.concat(dfs, keys=index)
+result.index.names = ['Dataset','Metric']
+
+result.to_csv(ePath + 'AllErrors.csv')
