4a60a0edbc6e115bafbc8fa3c7d1a2b123671df0
Jaime Farina <fffarinha@gmail.com>
2022-05-01 21:27:13 +1200

    Added metric plot with country info

--- errorSum.py
+++ errorSum.py
@@ -31,7 +31,6 @@ aErrors.to_csv(rPath + 'AllErrors.csv')
 
 models = ['arima', 'prophet', 'lstm']
 labels = ['Weeks','Metric']
-# lineStyle = ['o-C0']
 lineStyle = ['o-C0','o-C1','o-C2']
 Models.Models.plots_path = rPath
 
@@ -49,8 +48,24 @@ for c in countries:
         legends.append(m.upper())
         title = 'Metric for {country} over weeks'.format(country=c)
     Models.Models.plot(Models.Models,xData,yData,lineStyle,legends,labels,fileName,title)
-        
-  
+
+
+lineStyle = ['o-C0','o-C1','o-C2','o-C3','o-C4','o-C5']
+for m in models:
+    xData = []
+    yData = []
+    legends = []
+    fileName = '{model}.png'.format(model=m)
+    for c in countries:
+        wsmc = wsm.loc[wsm['Countries']==c]
+        xData.append(wsmc[m].index)
+        yData.append(wsmc[m].values)
+        legends.append(c)
+        title = '{Model} Metric for every country over weeks'.format(Model=m.upper())
+    Models.Models.plot(Models.Models,xData,yData,lineStyle,legends,labels,fileName,title)
+
+
+# lineStyle = ['o-C0']
 # for c in countries:
 #     wsmc = wsm.loc[wsm['Countries']==c]
 #     for m in models:
